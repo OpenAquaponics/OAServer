b4cb15d9ddb0dcd36a4823f03fb794b9384f6fb8
jeliser <nestinator@gmail.com>
2012-12-26 13:48:03 +0100

    Got the initial build of JSON running with the unittest application.  It can parse the input file and extract data fields.  Going to use the OANode.json to configure OANode on startup.

--- unittest/unittest.cpp
+++ unittest/unittest.cpp
@@ -1,6 +1,10 @@
 
 #include <list>
+#include <iostream>
+#include <fstream>
 #include "OANode.h"
+
+#include "json/json.h"
 
 using namespace std;
 
@@ -10,9 +14,38 @@ using namespace std;
 int main(int argc, char **argv) {
 /****************************************/
   /* Variable Declaration */
-  std::list< boost::shared_ptr<OANode> > lpClients;
 
+  printf("INFO: Reading JSON files\n");
+  Json::Value root;
+  Json::Reader reader;
+  Json::Value::Members members;
+  ifstream infile;
+  infile.open("./OANode.json", ifstream::in);
+  if(!reader.parse(infile, root, false)) {
+    cout << "Failed to parse JSON file: " << reader.getFormatedErrorMessages() << endl;
+    return(0);
+  }
+
+  std::cout << root.toStyledString();
+
+  /* Build OANodeCfg */
+  if(root.isMember("OANodeCfg")) {
+    char param[][32] = {{"mAcctHash"}, {"mSystemId"}, {"mDeviceId"}, {"mOAServerIp"}, {"mOAServerPort"}, {"Hello"}};
+    for(int i = 0; i < 6; i++) {
+      if(!root["OANodeCfg"].isMember(param[i])) {
+        cout << "ERR: Missing required param: " << param[i] << endl;
+      }
+    }
+
+    /* OANodeCfg mCfg = new OANodeCfg(); */
+    for(int i = 0; i < 5; i++) {
+      cout << "Extracted: " << root["OANodeCfg"].get(param[i], "").asString() << endl;
+    }
+  }
+
+#if 0
   printf("INFO: Spawning the OANodes\n");
+  std::list< boost::shared_ptr<OANode> > lpClients;
   for(int i = 0; i < CLIENT_CNT; i++) {
     lpClients.push_back(boost::shared_ptr<OANode>(new OANode((char*)"127.0.0.1", 50000, (char*)"", 2)));
   }
@@ -23,31 +56,10 @@ int main(int argc, char **argv) {
     }
   }
 
-#if 0
-  /* Traverse the vector to find the matching fd */
-    /* Found the matching socket */
-    if((*pIter).get()->GetSocketFd() == mFd) {
-
-      pIter = vpSockList.erase(pIter);
-
-      /* Reset the select variables */
-      FD_ZERO(&mListFds);
-      for(int i = 0; i < vpSockList.size(); i++) {
-        FD_SET(vpSockList[i].get()->GetSocketFd(), &mListFds);
-      }
-
-      return(0);
-    }
-    else {
-      pIter++;
-    }
-  }
+  lpClients.erase(lpClients.begin(), lpClients.end());
 #endif
 
 
-  lpClients.erase(lpClients.begin(), lpClients.end());
-
-  /* Have the sockets send a bunch of data */
 
   return(0);
 }
